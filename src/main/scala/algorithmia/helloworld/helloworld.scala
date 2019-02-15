1e06e912e1dd283509a5a36cfb18e3829786bd54
Adrian Harris <harrih@C02SQ033G8WL.local>
2019-02-15 15:21:54 +0000

    Jenkins build and test pipeline

--- src/main/scala/algorithmia/helloworld/helloworld.scala
+++ src/main/scala/algorithmia/helloworld/helloworld.scala
@@ -1,10 +1,5 @@
 package algorithmia.helloworld
 
-import com.algorithmia._
-import com.algorithmia.algo._
-import com.algorithmia.data._
-import com.google.gson._
-
 class helloworld {
   def apply(input: String): String = {
     "Hello " + input
